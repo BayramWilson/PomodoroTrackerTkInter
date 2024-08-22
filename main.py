d56e6dbd42d32a5f5e53fedc2d4a0053aa339f6e
IlluminatisSon <121392126+BayramWilson@users.noreply.github.com>
2024-08-23 00:52:00 +0200

    UI Setup is done

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import emoji
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -13,13 +14,31 @@ LONG_BREAK_MIN = 20
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
+
 
 # ---------------------------- UI SETUP ------------------------------- #
+#Create Window
 window = Tk()
 window.title("Pomodoro")
-
-canvas = Canvas()
-
+window.config(padx=100, pady=50, bg=YELLOW)
+# Create Canvas
+canvas = Canvas(width=200, height=250, bg=YELLOW, highlightthickness=0)
+image = PhotoImage(file="tomato.png")
+canvas.create_image(100, 112, image=image)
+canvas.create_text(100,130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+canvas.grid(column=1, row=1)
+# Create Label (Timer)
+timer_label = Label(text="Timer", bg=YELLOW, fg=GREEN, font=(FONT_NAME, 50, "bold"))
+timer_label.grid(column=1, row=0)
+# Create button Start
+start_button = Button(text="Start",  width=8)
+start_button.grid(column=0, row=2)
+# Create button Start
+reset_button = Button(text="Reset", width= 8)
+reset_button.grid(column=2, row=2)
+# Add Check Marks
+check_marks = Label(text="✅", fg=GREEN, bg=YELLOW)
+check_marks.grid(column=1, row=3)
 
 window.mainloop()
