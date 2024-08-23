4cfbbec5907ed32dd2b2834859ffe2355d427a10
IlluminatisSon <121392126+BayramWilson@users.noreply.github.com>
2024-08-23 16:21:41 +0200

    included timer, start button, reset button mechanisms aswell the style and different timer stages

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from tkinter import *
-import emoji
+import math
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -9,36 +9,90 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
-
+reps = 0
+timer = "None"
 # ---------------------------- TIMER RESET ------------------------------- # 
 
-# ---------------------------- TIMER MECHANISM ------------------------------- # 
 
+def reset_timer():
+    window.after_cancel(timer)
+    canvas.itemconfig(timer_text, text="00:00")
+    timer_label.config(text="Timer", bg=YELLOW, fg=GREEN)
+    check_marks.config(text="")
+    global reps
+    reps = 0
+# ---------------------------- TIMER MECHANISM ------------------------------- #
+
+
+def start_timer():
+    global reps
+    reps += 1
+
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
+
+    if reps % 8 == 0:
+        timer_label.config(text="LONG BREAK", fg=RED)
+        count_down(long_break_sec)
+    elif reps % 2 == 0:
+        timer_label.config(text="SHORT BREAK", fg=PINK)
+        count_down(short_break_sec)
+    else:
+        timer_label.config(text="WORK", fg=GREEN)
+        count_down(work_sec)
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 
 
-# ---------------------------- UI SETUP ------------------------------- #
-#Create Window
+def count_down(count):
+    """every second count down"""
+    count_min = math.floor(count / 60)
+    count_sec = count % 60
+    # if count_sec == 0:
+    #     count_sec = "00"
+    if count_sec < 10:
+        count_sec = f"0{count_sec}"
+    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
+    if count > 0:
+        global timer
+        timer = window.after(100, count_down, count - 1)
+    else:
+        start_timer()
+        marks = ""
+        work_sessions = math.floor(reps/2)
+        for _ in range(work_sessions):
+            marks += "✅"
+        check_marks.config(text=marks)
+# --------------------------- UI SETUP ------------------------------- #
+# Create Window
+
+
 window = Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
+
+
 # Create Canvas
 canvas = Canvas(width=200, height=250, bg=YELLOW, highlightthickness=0)
 image = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=image)
-canvas.create_text(100,130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.grid(column=1, row=1)
+# call function
 # Create Label (Timer)
 timer_label = Label(text="Timer", bg=YELLOW, fg=GREEN, font=(FONT_NAME, 50, "bold"))
 timer_label.grid(column=1, row=0)
 # Create button Start
-start_button = Button(text="Start",  width=8)
+
+start_button = Button(text="Start",  width=8, command=start_timer)
 start_button.grid(column=0, row=2)
+
 # Create button Start
-reset_button = Button(text="Reset", width= 8)
+reset_button = Button(text="Reset", width=8, command=reset_timer)
 reset_button.grid(column=2, row=2)
 # Add Check Marks
-check_marks = Label(text="✅", fg=GREEN, bg=YELLOW)
+check_marks = Label(fg=GREEN, bg=YELLOW)
 check_marks.grid(column=1, row=3)
+
 
 window.mainloop()
