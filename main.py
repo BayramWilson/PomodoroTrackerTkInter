bc5007a68ed63bc59a337bf9b2ed44a7bc5664cc
Rammo <bayram.wilson04@gmail.com>
2024-09-03 00:04:20 +0200

    changed style

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ from tkinter import *
 import math
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
+BLACK = "#000000"
 RED = "#e7305b"
-GREEN = "#9bdeac"
-YELLOW = "#f7f5dd"
+GREEN = "#14F195"
+SOLANA = "#9945FF"
 FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
@@ -17,7 +18,7 @@ timer = "None"
 def reset_timer():
     window.after_cancel(timer)
     canvas.itemconfig(timer_text, text="00:00")
-    timer_label.config(text="Timer", bg=YELLOW, fg=GREEN)
+    timer_label.config(text="Timer", bg=SOLANA, fg=GREEN)
     check_marks.config(text="")
     global reps
     reps = 0
@@ -55,13 +56,13 @@ def count_down(count):
     canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
     if count > 0:
         global timer
-        timer = window.after(100, count_down, count - 1)
+        timer = window.after(1000, count_down, count - 1)
     else:
         start_timer()
         marks = ""
         work_sessions = math.floor(reps/2)
         for _ in range(work_sessions):
-            marks += "✅"
+            marks += "✓"
         check_marks.config(text=marks)
 # --------------------------- UI SETUP ------------------------------- #
 # Create Window
@@ -69,18 +70,18 @@ def count_down(count):
 
 window = Tk()
 window.title("Pomodoro")
-window.config(padx=100, pady=50, bg=YELLOW)
+window.config(padx=100, pady=50, bg=SOLANA)
 
 
 # Create Canvas
-canvas = Canvas(width=200, height=250, bg=YELLOW, highlightthickness=0)
-image = PhotoImage(file="tomato.png")
+canvas = Canvas(width=200, height=250, bg=SOLANA, highlightthickness=0)
+image = PhotoImage(file="sol.png")
 canvas.create_image(100, 112, image=image)
 timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
 canvas.grid(column=1, row=1)
 # call function
 # Create Label (Timer)
-timer_label = Label(text="Timer", bg=YELLOW, fg=GREEN, font=(FONT_NAME, 50, "bold"))
+timer_label = Label(text="Timer", bg=SOLANA, fg=GREEN, font=(FONT_NAME, 50, "bold"))
 timer_label.grid(column=1, row=0)
 # Create button Start
 
@@ -91,7 +92,7 @@ start_button.grid(column=0, row=2)
 reset_button = Button(text="Reset", width=8, command=reset_timer)
 reset_button.grid(column=2, row=2)
 # Add Check Marks
-check_marks = Label(fg=GREEN, bg=YELLOW)
+check_marks = Label(fg=BLACK, bg=SOLANA)
 check_marks.grid(column=1, row=3)
 
 
